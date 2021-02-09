ad3a0324850f72e76afad737517ebda0e2637962
Alexandre HEIM <aheim@i-s-m.com>
2021-02-09 16:05:10 +0100

    add logger

---
+++ logger/logger.go
@@ -0,0 +1,40 @@
+package logger
+
+import (
+	"context"
+	"github.com/sirupsen/logrus"
+)
+
+var contextKey struct{} // context key
+
+// New creates a new loggger
+func New(level string, fields ...logrus.Fields) *logrus.Entry {
+	logger := logrus.New()
+	entry := logrus.NewEntry(logger)
+	for _, f := range fields {
+		entry = entry.WithFields(f)
+	}
+
+	askedLevel, err := logrus.ParseLevel(level)
+	if err != nil {
+		askedLevel = logrus.TraceLevel
+		entry.WithField("level", level).Warn("bad log level")
+	}
+	logger.SetLevel(askedLevel)
+
+	return entry
+}
+
+// WithLogger adds the logger as context value
+func WithLogger(ctx context.Context, entry *logrus.Entry) context.Context {
+	return context.WithValue(ctx, contextKey, entry)
+}
+
+// FromContext returns the logger set with WithLogger or nil
+func FromContext(ctx context.Context) *logrus.Entry {
+	entry, ok := ctx.Value(contextKey).(*logrus.Entry)
+	if !ok {
+		return nil
+	}
+	return entry
+}
