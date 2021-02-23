3eb1f56af9ba9f6ea5824055be5d24d80158585a
Alexandre HEIM <aheim@i-s-m.com>
2021-02-23 15:08:46 +0100

    put remote-acces binaries in bin folder
    more fiels

---
+++ events/cmd/remote-access/build.sh
@@ -0,0 +1,7 @@
+GOOS=linux GOARCH=amd64 go build -trimpath -o ./bin/remote-access_linux_amd64
+GOOS=linux GOARCH=arm GOARM=7 go build -trimpath -o ./bin/remote-access_linux_arm
+GOOS=darwin GOARCH=amd64 go build -trimpath -o ./bin/remote-access_darwin_amd64
+GOOS=windows GOARCH=amd64 go build -trimpath -o ./bin/remote-access_windows.exe
+
+rsync -avP ./bin/remote-access_linux_amd64 cloud.plugis.com:
+rsync -avP ./bin/remote-access_linux_arm colorbeam@colorbeam:
