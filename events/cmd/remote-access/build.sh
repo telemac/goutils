5099e30c5940828ed660cb8b2c30c08c329d07a9
Alexandre HEIM <aheim@i-s-m.com>
2021-03-05 18:34:39 +0100

    correct build.sh for remote-access

--- events/cmd/remote-access/build.sh
+++ events/cmd/remote-access/build.sh
@@ -1,8 +1,12 @@
-GOOS=linux GOARCH=amd64 go build -trimpath -o ./bin/remote-access_linux_amd64
-GOOS=linux GOARCH=arm GOARM=7 go build -trimpath -o ./bin/remote-access_linux_arm
-GOOS=darwin GOARCH=amd64 go build -trimpath -o ./bin/remote-access_darwin_amd64
-GOOS=windows GOARCH=amd64 go build -trimpath -o ./bin/remote-access_windows.exe
+BINARY=remote-access
 
-rsync -avP ./bin/remote-access_linux_amd64 cloud.plugis.com:
-rsync -avP ./bin/remote-access_linux_amd64 colorbeam@cbna:
-rsync -avP ./bin/remote-access_linux_arm colorbeam@colorbeam:
+GOOS=linux GOARCH=amd64 go build -trimpath -o ./bin/linux/amd64/$BINARY
+GOOS=linux GOARCH=arm GOARM=7 go build -trimpath -o ./bin/linux/arm/$BINARY
+GOOS=darwin GOARCH=amd64 go build -trimpath -o ./bin/darwin/amd64/$BINARY
+GOOS=windows GOARCH=amd64 go build -trimpath -o ./bin/windows/amd64/$BINARY
+md5 ./bin/linux/amd64/$BINARY > ./bin/linux/amd64/$BINARY.md5
+md5 ./bin/linux/arm/$BINARY > ./bin/linux/arm/$BINARY.md5
+md5 ./bin/darwin/amd64/$BINARY > ./bin/darwin/amd64/$BINARY.md5
+md5 ./bin/windows/amd64/$BINARY > ./bin/windows/amd64/$BINARY.md5
+
+rsync -avP ./bin/ update.plugis.com:~/data/update.plugis.com/www/
