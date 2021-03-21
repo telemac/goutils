e1c499b3f8336ba24931034c11146029c884a7b3
Alexandre HEIM <aheim@i-s-m.com>
2021-03-21 18:40:42 +0100

    add FileExists in package files

--- files/fileexists.go
+++ files/fileexists.go
@@ -1,1 +1,12 @@
 package files
+
+import "os"
+
+// FileExists returns true if the file exists, false if not
+func FileExists(name string) (bool, error) {
+	_, err := os.Stat(name)
+	if err != nil && os.IsNotExist(err) {
+		return false, nil
+	}
+	return err == nil, err
+}
