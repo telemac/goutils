3f4735386fc3aa8383c5db5e6d9ac7670398fc32
Alexandre HEIM <aheim@i-s-m.com>
2021-03-24 22:37:01 +0100

    WIP: postgres helper functions

--- postgres/createdatabase.go
+++ postgres/createdatabase.go
@@ -1,1 +1,8 @@
 package postgres
+
+import "fmt"
+
+// CreateDatabaseIfNotExists returns the postgresql syntax equivalent to mysql CREATE DATABASE IF NOT EXISTS.
+func CreateDatabaseIfNotExists(databaseName string) string {
+	return fmt.Sprintf("SELECT 'CREATE DATABASE %s' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '%s')\\gexec\n", databaseName, databaseName)
+}
