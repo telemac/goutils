89d512ba6a942077c6e7e873e12bad5560d8e676
Alexandre HEIM <aheim@i-s-m.com>
2021-04-09 12:03:05 +0200

    CloudEventHandler uses cloudevents.Event type now

--- natsevents/receiver.go
+++ natsevents/receiver.go
@@ -1,6 +1,7 @@
 package natsevents
 
 import (
+	cloudevents "github.com/cloudevents/sdk-go/v2"
 	"github.com/cloudevents/sdk-go/v2/event"
 )
 
@@ -8,7 +9,7 @@ import (
 // must return as fast as possible.
 // event can be nil, in this case the payload will be set
 // if event is not nil (properly decoded) the payload is nil
-type CloudEventHandler func(topic string, event *event.Event, payload []byte, err error) (*event.Event, error)
+type CloudEventHandler func(topic string, event *cloudevents.Event, payload []byte, err error) (*cloudevents.Event, error)
 
 // CloudEventReceiver allows to receive cloud events
 type CloudEventReceiver interface {
