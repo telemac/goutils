a46020985a174b731171cdd05d54223c73f6a604
Alexandre HEIM <aheim@i-s-m.com>
2021-04-21 18:46:05 +0200

    add command line parameters to config
    log level and config folder

--- natsservice/config.go
+++ natsservice/config.go
@@ -1,7 +1,11 @@
 package natsservice
 
 import (
+	"flag"
+	"fmt"
 	"github.com/jinzhu/configor"
+	"os"
+	"path"
 )
 
 type PostgresConfig struct {
@@ -25,16 +29,45 @@ type Config struct {
 
 	Servers []struct {
 		Url string `default:"nats://nats1.plugis.com:443"`
-		Sni bool   `default:"false"`
+		Sni bool   `default:"true"`
 	}
 
 	Postgres PostgresConfig
 
 	Mysql MysqlConfig
+
+	CommandLineParams CommonCommandLineParams
+}
+
+// CommandLineParams holds the common, non generic command line parameters
+type CommonCommandLineParams struct {
+	Log          string
+	ConfigFolder string
+}
+
+// ParseCommonCommandLineParams sets the command line parameters, or their default values
+func ParseCommonCommandLineParams(params *CommonCommandLineParams) {
+	flag.StringVar(&params.Log, "log", "info", "log level")
+	flag.StringVar(&params.ConfigFolder, "config", "./", "folder containing configuration files")
+	flag.Parse()
 }
 
 func LoadConfig(files ...string) (Config, error) {
 	var config Config
-	err := configor.Load(&config, files...)
+	ParseCommonCommandLineParams(&config.CommandLineParams)
+
+	// append config folder to given files
+	var configFiles []string
+	for _, file := range files {
+		// check if file exists.
+		fullPath := path.Join(config.CommandLineParams.ConfigFolder, file)
+		_, err := os.Stat(fullPath)
+		if err != nil {
+			return config, fmt.Errorf("load config file %s : %w", fullPath, err)
+		}
+		configFiles = append(configFiles, fullPath)
+	}
+
+	err := configor.Load(&config, configFiles...)
 	return config, err
 }
