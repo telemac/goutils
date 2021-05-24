a290ca37014512c8f4135bf1123d9fe671c5afe5
Alexandre HEIM <aheim@i-s-m.com>
2021-05-24 18:40:37 +0200

    create variable package

--- variable/variable.go
+++ variable/variable.go
@@ -1,1 +1,26 @@
 package variable
+
+import (
+	cloudevents "github.com/cloudevents/sdk-go/v2"
+	"github.com/telemac/goutils/natsevents"
+	"time"
+)
+
+// Variable represents represents a variable value
+type Variable struct {
+	Name      string      `json:"name"`
+	Type      string      `json:"type"`
+	Value     interface{} `json:"value"`
+	Timestamp time.Time   `json:"ts,omitempty"`
+	Comment   string      `json:"comment,omitempty"`
+}
+
+// Variables is an array of variable values
+type Variables []Variable
+
+// cloud event variable set type
+const CESetType = "com.plugis.variable.set"
+
+func NewVariableSetEvent(variables Variables) *cloudevents.Event {
+	return natsevents.NewEvent("", CESetType, variables)
+}
