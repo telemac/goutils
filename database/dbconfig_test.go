58a3f1c302b2272f56bcac8d8bf26caadf17d373
Alexandre HEIM <aheim@i-s-m.com>
2022-04-22 14:27:31 +0200

    add MySQLConnect test

--- database/dbconfig_test.go
+++ database/dbconfig_test.go
@@ -19,3 +19,18 @@ func TestLoadMySQLConfig(t *testing.T) {
 	assert.NoError(err)
 	assert.Equal(&DBConfig{Host: "myHost", Database: "myDatabase", User: "myUser", Password: "myPassword", Port: 3306}, got)
 }
+
+func TestMySQLConnect(t *testing.T) {
+	assert := assert.New(t)
+
+	config := &DBConfig{
+		Host:     "127.0.0.1",
+		User:     "root",
+		Password: "telemac",
+		Database: "frontal",
+		Port:     3306,
+	}
+	db, err := MySQLConnect(config)
+	assert.NoError(err)
+	_ = db
+}
