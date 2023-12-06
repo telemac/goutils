15b3ebb22a9cf603efd0829f46d65c1c5b706bd4
Alexandre HEIM <aheim@i-s-m.com>
2023-12-06 11:10:28 +0100

    add task.Interrupt()

--- task/abort.go
+++ task/abort.go
@@ -5,4 +5,10 @@ import "syscall"
 // Abort aborts the process by sending a SIGABRT signal
 func Abort() {
 	syscall.Kill(syscall.Getpid(), syscall.SIGABRT)
+	syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+}
+
+// Interrupt aborts the process by sending a SIGINT signal
+func Interrupt() {
+	syscall.Kill(syscall.Getpid(), syscall.SIGINT)
 }
