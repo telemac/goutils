a99949027b7da3352ad8d1cddacb53d44f81cf0c
Alexandre HEIM <aheim@i-s-m.com>
2024-03-15 08:59:42 +0100

    disable cgo in remote-access

--- events/cmd/remote-access/build.sh
+++ events/cmd/remote-access/build.sh
@@ -1,9 +1,9 @@
 BINARY=remote-access
 
-GOOS=linux GOARCH=amd64 go build -trimpath -o ./bin/linux/amd64/$BINARY
-GOOS=linux GOARCH=arm GOARM=7 go build -trimpath -o ./bin/linux/arm/$BINARY
-GOOS=darwin GOARCH=amd64 go build -trimpath -o ./bin/darwin/amd64/$BINARY
-GOOS=windows GOARCH=amd64 go build -trimpath -o ./bin/windows/amd64/$BINARY.exe
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -trimpath -o ./bin/linux/amd64/$BINARY
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -trimpath -o ./bin/linux/arm/$BINARY
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -trimpath -o ./bin/darwin/amd64/$BINARY
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -trimpath -o ./bin/windows/amd64/$BINARY.exe
 md5 ./bin/linux/amd64/$BINARY > ./bin/linux/amd64/$BINARY.md5
 md5 ./bin/linux/arm/$BINARY > ./bin/linux/arm/$BINARY.md5
 md5 ./bin/darwin/amd64/$BINARY > ./bin/darwin/amd64/$BINARY.md5
